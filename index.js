301bb03f13debd2e8f87fd3d26108c21241ceebe
isabel.sleigman <sleimusic@gmail.com>
2022-04-08 17:15:28 -0300

    tratamento de erro

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const chalk = require('chalk')
 const fs = require('fs')
 
+function handleError(error) {
+    throw new Error(chalk.red(error.code, 'não há arquivo no caminho'))
+}
+
 function getFile(pathFile) {
     const encoding = 'utf8'
-    fs.readFile(pathFile, encoding, (_, texto) => {
+    fs.readFile(pathFile, encoding, (error, texto) => {
+        if (error) {
+            handleError(error)
+        }
         console.log(chalk.green(texto))
     })
 }
