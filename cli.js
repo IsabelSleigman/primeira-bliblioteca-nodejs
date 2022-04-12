6d5760bc736cdf65595c6acd90aa73fc25cdab24
isabel.sleigman <sleimusic@gmail.com>
2022-04-12 14:47:49 -0300

    refatorando cli

--- cli.js
+++ cli.js
@@ -1,11 +1,16 @@
 const chalk = require('chalk')
+const validaURLs = require('./http-validacao')
 const getFile = require('./index');
 
 const caminho = process.argv;
 
 async function processaTexto(caminhoArquivo) {
     const resultado = await getFile(caminhoArquivo[2])
-    console.log(chalk.yellow('Lista de links'), resultado)
+    if (caminho[3] === 'validar') {
+        console.log(chalk.yellow('Links validados'), await validaURLs(resultado))
+    } else {
+        console.log(chalk.yellow('Lista de links'), resultado)
+    }
 }
 
 processaTexto(caminho)
