318dcf7bdfd9e272759dfc80133adaab17382e10
isabel.sleigman <sleimusic@gmail.com>
2022-04-12 15:39:21 -0300

    juntando arrays de links e status

--- http-validacao.js
+++ http-validacao.js
@@ -1,23 +1,43 @@
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+const chalk = require('chalk')
+
+function handleError(erro) {
+    throw new Error(erro.mensage)
+}
 
 async function checaStatus(arrayURLs) {
+    try {
+        const arrayStatus = await Promise
+            .all(arrayURLs
+                .map(async url => {
+                    const res = await fetch(url)
+                    return res.status
+                }))
 
-    const arrayStatus = await Promise.all(arrayURLs.map(async url => {
-        const res = await fetch(url)
-        return res.status
-    }))
+        return arrayStatus
 
-    return arrayStatus
+    } catch (error) {
+        handleError(error)
+    }
 }
 
 function geraArrayURLs(arrayLinks) {
-    return arrayLinks.map(link => Object.values(link).join())
+    return arrayLinks
+        .map(link => Object
+            .values(link).join())
 }
 
 async function validaURLs(arrayLinks) {
     const links = geraArrayURLs(arrayLinks)
     const statusLinks = await checaStatus(links)
-    return statusLinks
+
+    // spread operator
+    const resultados = arrayLinks.map((objeto, indice) => ({
+        ...objeto,
+        statusLink: statusLinks[indice]
+    }))
+
+    return resultados
 }
 
 module.exports = validaURLs
