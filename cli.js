93c0572642558300fa14cf3506866adb6e201c91
isabel.sleigman <sleimusic@gmail.com>
2022-04-12 13:40:56 -0300

    melhorando logica

--- cli.js
+++ cli.js
@@ -1,6 +1,11 @@
+const chalk = require('chalk')
 const getFile = require('./index');
 
 const caminho = process.argv;
 
+async function processaTexto(caminhoArquivo) {
+    const resultado = await getFile(caminhoArquivo[2])
+    console.log(chalk.yellow('Lista de links'), resultado)
+}
 
-console.log(getFile(caminho[2]))
+processaTexto(caminho)
