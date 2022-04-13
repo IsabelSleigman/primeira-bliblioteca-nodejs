231c568a8e5c447c07264230a9f446260b5febdd
isabel.sleigman <sleimusic@gmail.com>
2022-04-13 11:18:43 -0300

    finalizando testes

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,27 @@
-const getFile = require('../index')
+const pegaArquivo = require('../index');
 
-test('deve ser uma função', () => {
-    expect(typeof getFile).toBe('function');
+const arrayResult = [
+  {
+    FileList: 'https://developer.mozilla.org/pt-BR/docs/Web/API/FileList'
+  }
+]
+
+describe('pegaArquivo::', () => {
+  it('deve ser uma função', () => {
+    expect(typeof pegaArquivo).toBe('function');
+  })
+  it('deve retornar array com resultados', async () => {
+    const resultado = await pegaArquivo('/home/juliana/Documents/alura/lib-markdown/test/arquivos/texto1.md') 
+    expect(resultado).toEqual(arrayResult)
+  })
+  it('deve retornar mensagem "não há links"', async () => {
+    const resultado = await pegaArquivo('/home/juliana/Documents/alura/lib-markdown/test/arquivos/texto1_semlinks.md')
+    expect(resultado).toBe('não há links');
+  })
+  it('deve lançar um erro na falta de arquivo', () => {
+    async function capturaErro() {
+      await pegaArquivo('/home/juliana/Documents/alura/lib-markdown/test/arquivos')
+      expect(capturaErro).toThrowError(/não há arquivo no caminho/)
+    }
+  })
 })
